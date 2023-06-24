01dddf577944983facdef312c51964efc86fe89c
Andy Phan <phan2410@gmail.com>
2023-06-24 23:36:14 +0700

    [LinuxMint] Update setup.sh

--- LinuxMint/setup.sh
+++ LinuxMint/setup.sh
@@ -1,5 +1,7 @@
 # Most of things are like in Ubuntu
 
+# TRY USE DEFAULT before installing anything
+
 # [Unikey]
 # Go to LM Menu (press Super) > Search for Input method 
 #  > Choose Vietnamese > process as instruction there
