fb7fab70c15dcc48958c2275a40c283c4dfc8a99
Andy Phan <phan2410@gmail.com>
2023-06-25 11:32:18 +0700

    Update setup.sh

--- LinuxMint/setup.sh
+++ LinuxMint/setup.sh
@@ -8,3 +8,7 @@
 
 # [Docker]
 # https://docs.docker.com/engine/install/ubuntu/#set-up-the-repository
+# MUST CHECK based on Ubuntu version, here I use jammy, so use this command instead
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
