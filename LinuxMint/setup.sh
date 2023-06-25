ad363cf2ba6b4a1341e8697563261536212065d2
Andy Phan <phan2410@gmail.com>
2023-06-25 13:28:30 +0700

    [LinuxMint] config wol

--- LinuxMint/setup.sh
+++ LinuxMint/setup.sh
@@ -2,11 +2,11 @@
 
 # TRY USE DEFAULT before installing anything
 
-# [Unikey]
+[UNIKEY]
 # Go to LM Menu (press Super) > Search for Input method 
 #  > Choose Vietnamese > process as instruction there
 
-# [Docker]
+[DOCKER]
 # https://docs.docker.com/engine/install/ubuntu/#set-up-the-repository
 # MUST CHECK based on Ubuntu version, here I use jammy, so use this command instead
 echo \
@@ -14,8 +14,40 @@ echo \
   jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 # REMEMBER to do POST INSTALLATION Guide
 
-# Install Docker desktop GUI
+[Docker desktop GUI]
 # https://docs.docker.com/desktop/release-notes/
 # enable: systemctl --user enable docker-desktop
 # and configure the disk image to /home/fan/.docker
+
+[docker-compose]
 # don't Install docker-compose from apt, just use bash script or check integration in docker-desktop
+
+
+[WAKE ON LAN]
+# https://necromuralist.github.io/posts/enabling-wake-on-lan/
+
+sudo apt install ethtool
+ip a # => try get mac address from enp#s0 interface
+
+sudo ethtool enp7s0
+which ethtool # => copy binary path 
+
+sudo nano /etc/systemd/system/wol.service
+# Paste following content
+[Unit]
+Description=Enable Wake On Lan
+
+[Service]
+Type=oneshot
+ExecStart = </path/to/ethtool> --change <enpXs0> wol g
+
+[Install]
+WantedBy=basic.target
+# end
+# Enable the service
+sudo systemctl daemon-reload
+sudo systemctl enable wol.service
+sudo systemctl start wol.service
+systemctl status wol
+
+
