3fea691bdfc8dce8e982450be3963dbad91b82b8
Andy Phan <phan2410@gmail.com>
2023-06-25 11:28:47 +0700

    Update setup.sh

--- LinuxMint/setup.sh
+++ LinuxMint/setup.sh
@@ -5,3 +5,6 @@
 # [Unikey]
 # Go to LM Menu (press Super) > Search for Input method 
 #  > Choose Vietnamese > process as instruction there
+
+# [Docker]
+# https://docs.docker.com/engine/install/ubuntu/#set-up-the-repository
